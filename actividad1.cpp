0516d3267901c21d350086722680493cb0e5ca3e
oli <A01804703@tec.mx>
2025-11-11 09:22:10 -0600

    act 1

---
+++ actividad1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float base = 5.5;
+    float altura = 3.2;
+    float area = base * altura;
+
+    cout << "El area del rectangulo es: " << area << endl;
+    return 0;
+}
